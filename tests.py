8dd7afdc123c163bc9ce9a67cefd3ff8a09d04e1
Mizuki Sato <mizki.sato.0025@gmail.com>
2019-02-02 16:04:22 +0900

    格子点の等価性の比較をテストした。

--- tests.py
+++ tests.py
@@ -10,3 +10,20 @@ import gridpoint
 ])
 def test_string_notation(x, y, expected):
     assert str(gridpoint.GridPoint(x, y)) == expected
+
+
+@pytest.mark.parametrize("x, y", [
+    (4, 7),
+    (-4, -7),
+])
+def test_equality(x, y):
+    assert gridpoint.GridPoint(x, y) == gridpoint.GridPoint(x, y)
+
+
+@pytest.mark.parametrize("a, b", [
+    ((4, 7), (-4, 7)),
+    ((4, 7), (4, -7)),
+    ((4, 7), (-4, -7)),
+])
+def test_non_equality(a, b):
+    assert gridpoint.GridPoint(*a) != gridpoint.GridPoint(*b)
