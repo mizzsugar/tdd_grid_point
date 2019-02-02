00e54bc6e93caad6c2af1908bcc60a4deab79849
Naruse Motoki <motoki@naru.se>
2019-02-02 15:48:14 +0900

    文字列表記のテストケースを追加

--- tests.py
+++ tests.py
@@ -1,5 +1,12 @@
+import pytest
+
 import gridpoint
 
 
-def test_string_notation():
-    assert str(gridpoint.GridPoint(4, 7)) == "(4, 7)"
+@pytest.mark.parametrize("x, y, expected", [
+    (4, 7, "(4, 7)"),
+    (-4, -7, "(-4, -7)"),
+    (0, 0, "(0, 0)"),
+])
+def test_string_notation(x, y, expected):
+    assert str(gridpoint.GridPoint(x, y)) == expected
