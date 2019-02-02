1899e98471ae801a954dcb593737c301ca584b1d
Mizuki Sato <mizki.sato.0025@gmail.com>
2019-02-02 17:33:46 +0900

    格子点集合に格子点を含まれているかのテストを書いた。

--- tests.py
+++ tests.py
@@ -42,3 +42,25 @@ class TestGridPoint:
     ])
     def test_is_not_next_to(self, a, b) -> bool:
         assert not gridpoint.GridPoint(*a).is_next_to(gridpoint.GridPoint(*b))
+
+
+class TestGridPoints:
+    @pytest.mark.parametrize("a, b", [
+        ((0, 0), (0, 1)),
+    ])
+    def test_contains(self, a, b) -> bool:
+        gridpoint_a = gridpoint.GridPoint(*a)
+        gridpoint_b = gridpoint.GridPoint(*b)
+        gridpoints = gridpoint.GridPoints(gridpoint_a, gridpoint_b)
+        assert gridpoint_a in gridpoints
+        assert gridpoint_b in gridpoints
+
+    @pytest.mark.parametrize("a, b, c", [
+        ((0, 0), (0, 1), (0, 2)),
+    ])
+    def test_not_contains(self, a, b, c) -> bool:
+        gridpoint_a = gridpoint.GridPoint(*a)
+        gridpoint_b = gridpoint.GridPoint(*b)
+        gridpoint_c = gridpoint.GridPoint(*c)
+        gridpoints = gridpoint.GridPoints(gridpoint_a, gridpoint_b)
+        assert gridpoint_c not in gridpoints
