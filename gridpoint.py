22f4f7bd06103c2c8c2e0f1c997688000b6cc077
Naruse Motoki <motoki@naru.se>
2019-02-02 16:43:04 +0900

    隣接判定のテストを追加
    隣接とはある点の上下左右の一方向に一点分動いた位置を言う

--- gridpoint.py
+++ gridpoint.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from typing import NamedTuple
 
 
@@ -7,3 +8,11 @@ class GridPoint(NamedTuple):
 
     def __str__(self) -> str:
         return f"({self.x}, {self.y})"
+
+    def is_next_to(self, other: GridPoint) -> bool:
+        """与えられた格子点が自身と隣接していればTrueを返す。それ以外はFalse。
+
+        隣接とはある点の上下左右の一方向に一点分動いた位置を言う。
+        """
+        # Stub
+        return True
