4f78f2e6b8921270cc9d8c5720af422d123e3c76
Mizuki Sato <mizki.sato.0025@gmail.com>
2019-02-02 17:13:36 +0900

    TestGridPointテストを作った
    TestGridPointsを導入するにあたり、TestGridPointと分けるための名前空間がほしかった。

--- tests.py
+++ tests.py
@@ -3,45 +3,42 @@ import pytest
 import gridpoint
 
 
-@pytest.mark.parametrize("x, y, expected", [
-    (4, 7, "(4, 7)"),
-    (-4, -7, "(-4, -7)"),
-    (0, 0, "(0, 0)"),
-])
-def test_string_notation(x, y, expected):
-    assert str(gridpoint.GridPoint(x, y)) == expected
+class TestGridPoint:
+    @pytest.mark.parametrize("x, y, expected", [
+        (4, 7, "(4, 7)"),
+        (-4, -7, "(-4, -7)"),
+        (0, 0, "(0, 0)"),
+    ])
+    def test_string_notation(self, x, y, expected):
+        assert str(gridpoint.GridPoint(x, y)) == expected
 
+    @pytest.mark.parametrize("x, y", [
+        (4, 7),
+        (-4, -7),
+    ])
+    def test_equality(self, x, y):
+        assert gridpoint.GridPoint(x, y) == gridpoint.GridPoint(x, y)
 
-@pytest.mark.parametrize("x, y", [
-    (4, 7),
-    (-4, -7),
-])
-def test_equality(x, y):
-    assert gridpoint.GridPoint(x, y) == gridpoint.GridPoint(x, y)
+    @pytest.mark.parametrize("a, b", [
+        ((4, 7), (-4, 7)),
+        ((4, 7), (4, -7)),
+        ((4, 7), (-4, -7)),
+    ])
+    def test_non_equality(self, a, b):
+        assert gridpoint.GridPoint(*a) != gridpoint.GridPoint(*b)
 
+    @pytest.mark.parametrize("a, b", [
+        ((0, 0), (0, 1)),
+        ((0, 0), (1, 0)),
+        ((0, -1), (0, 0)),
+    ])
+    def test_is_next_to(self, a, b) -> bool:
+        assert gridpoint.GridPoint(*a).is_next_to(gridpoint.GridPoint(*b))
 
-@pytest.mark.parametrize("a, b", [
-    ((4, 7), (-4, 7)),
-    ((4, 7), (4, -7)),
-    ((4, 7), (-4, -7)),
-])
-def test_non_equality(a, b):
-    assert gridpoint.GridPoint(*a) != gridpoint.GridPoint(*b)
-
-
-@pytest.mark.parametrize("a, b", [
-    ((0, 0), (0, 1)),
-    ((0, 0), (1, 0)),
-    ((0, -1), (0, 0)),
-])
-def test_is_next_to(a, b) -> bool:
-    assert gridpoint.GridPoint(*a).is_next_to(gridpoint.GridPoint(*b))
-
-
-@pytest.mark.parametrize("a, b", [
-    ((0, 0), (0, 0)),
-    ((1, 1), (0, 0)),
-    ((0, -1), (-1, 0)),
-])
-def test_is_not_next_to(a, b) -> bool:
-    assert not gridpoint.GridPoint(*a).is_next_to(gridpoint.GridPoint(*b))
+    @pytest.mark.parametrize("a, b", [
+        ((0, 0), (0, 0)),
+        ((1, 1), (0, 0)),
+        ((0, -1), (-1, 0)),
+    ])
+    def test_is_not_next_to(self, a, b) -> bool:
+        assert not gridpoint.GridPoint(*a).is_next_to(gridpoint.GridPoint(*b))
